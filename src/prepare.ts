b2fad0d5e1c4b61a5700d05dad51433aa84030ae
Raphaël Thériault <raphael_theriault@outlook.com>
2019-09-14 14:50:19 -0400

    Remove second rustfmt bin

--- src/prepare.ts
+++ src/prepare.ts
@@ -15,10 +15,15 @@ export default async function prepare(cargoPath: string) {
   }
 
   // Remove rustfmt and clippy if present
-  const rustfmtBin: string = await io.which("cargo-fmt", false);
-  if (rustfmtBin.length > 0) {
+  const rustfmtBin1: string = await io.which("cargo-fmt", false);
+  const rustfmtBin2: string = await io.which("rustfmt", false);
+  if (rustfmtBin1.length > 0) {
     core.debug("Removing existing rustfmt binary");
-    io.rmRF(rustfmtBin);
+    io.rmRF(rustfmtBin1);
+  }
+  if (rustfmtBin2.length > 0) {
+    core.debug("Removing existing rustfmt binary");
+    io.rmRF(rustfmtBin2);
   }
   const clippyBin: string = await io.which("cargo-clippy", false);
   if (clippyBin.length > 0) {
