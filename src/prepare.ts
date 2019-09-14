68904767b78a73c99be7b2072886b090dbb86e19
Raphaël Thériault <raphael_theriault@outlook.com>
2019-09-14 13:54:11 -0400

    Remove cargo subcommands in preparation step

--- src/prepare.ts
+++ src/prepare.ts
@@ -1,4 +1,5 @@
 import * as core from "@actions/core";
+import * as io from "@actions/io";
 import * as path from "path";
 
 export default async function prepare(cargoPath: string) {
@@ -9,7 +10,20 @@ export default async function prepare(cargoPath: string) {
   // Add cargo bin dir to path
   const envPath: string = process.env.PATH || "";
   if (!envPath.includes(cargoBinPath)) {
+    core.debug("Adding .cargo/bin to PATH");
     core.addPath(cargoBinPath);
+  }
+
+  // Remove rustfmt and clippy if present
+  const rustfmtBin: string = await io.which("cargo-fmt", false);
+  if (rustfmtBin.length > 0) {
+    core.debug("Removing existing rustfmt binary");
+    io.rmRF(rustfmtBin);
+  }
+  const clippyBin: string = await io.which("cargo-clippy", false);
+  if (clippyBin.length > 0) {
+    core.debug("Removing existing rustfmt clippy");
+    io.rmRF(clippyBin);
   }
 
   core.endGroup();
